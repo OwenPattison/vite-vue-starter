96736f37fc82a9efee9d43758c1300cd1076f0c9
Owen Pattison <opattison@aquilalearning.com>
2023-11-10 22:07:47 +0000

    adding initial build with tsx support

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,13 @@
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'Title',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    setup(props) {
+        return () => <h1>{props.name}</h1>;
+    },
+});
