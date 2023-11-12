cb6fbe5e6cb905c27b098e56ad77739479c161ac
Owen Pattison <opattison@aquilalearning.com>
2023-11-12 19:36:53 +0000

    Vitest install (#1)
    * adding vitest
    
    * adding testing library
    
    * formatting

---
+++ src/components/Welcome.test.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/vue';
+import { Welcome } from "./Welcome";
+
+describe('something truthy and falsy', () => {
+    it('true to be true', () => {
+        expect(true).toBe(true);
+    });
+
+    it('false to be false', () => {
+        expect(false).toBe(false);
+    });
+});
+
+describe('Welcome', () => {
+    it('renders heading', () => {
+        render(Welcome, { props: {
+            name: "Owen"
+        } });
+
+        screen.getByText('Hello Owen!')
+    });
+});
